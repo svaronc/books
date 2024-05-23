f2a6a8173f9913c9247c02788e3e836a5c1e9f35
svaronc <sebastianvaron96@gmail.com>
2024-05-23 05:31:12 -0500

    add  solution for a vector class

---
+++ eloquent_javascript/vectorClass.js
@@ -0,0 +1,18 @@
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+  }
+  minus(other) {
+    return new Vector(this.x - other.x, this.y - other.y);
+  }
+  get length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+}
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3))); // → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3))); // → Vector{x: -1, y: -1}
